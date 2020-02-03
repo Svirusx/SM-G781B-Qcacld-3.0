c9913a9143926fa1d4b4135516952fd9c7425ab1
Vulupala Shashank Reddy <vulupa@codeaurora.org>
2020-02-03 18:33:19 +0530

    qcacld-3.0: Register tx and rx ops for packet capture component
    Register tx and rx ops to send packet capture mode command and
    to receive mgmt offload handler for packet capture component.
    
    Change-Id: I70da00feff29399b98c5916eec17e26b62285db3
    CRs-Fixed: 2619321

--- components/target_if/pkt_capture/inc/target_if_pkt_capture.h
+++ components/target_if/pkt_capture/inc/target_if_pkt_capture.h
@@ -29,14 +29,19 @@
 #include "wlan_pkt_capture_public_structs.h"
 
 /**
- * target_if_set_packet_capture_mode() - set packet capture mode
- * @psoc: pointer to psoc object
- * @vdev_id: vdev id
- * @mode: mode to set
- *
- * Return: QDF_STATUS
+ * target_if_pkt_capture_register_rx_ops() - Register packet capture RX ops
+ * @rx_ops: packet capture component reception ops
+ * Return: None
  */
-QDF_STATUS target_if_set_packet_capture_mode(struct wlan_objmgr_psoc *psoc,
-					     uint8_t vdev_id,
-					     enum pkt_capture_mode mode);
+void
+target_if_pkt_capture_register_rx_ops(struct wlan_pkt_capture_rx_ops *rx_ops);
+
+/**
+ * target_if_pkt_capture_register_tx_ops() - Register packet capture TX ops
+ * @tx_ops: pkt capture component transmit ops
+ *
+ * Return: None
+ */
+void target_if_pkt_capture_register_tx_ops(struct wlan_pkt_capture_tx_ops
+					   *tx_ops);
 #endif /* _TARGET_IF_PKT_CAPTURE_H_ */
